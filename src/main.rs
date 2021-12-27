6f195f7611693ec7d1a9b3cbf589c7261c749a38
Yash Karandikar <nerdstep710@gmail.com>
2021-12-27 11:24:31 -0600

    Create basic IRC loop

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::{env, sync::Arc};
 
 use serenity::{
     async_trait,
+    futures::StreamExt,
     http::Http,
     model::{
         channel::Message,
@@ -9,6 +10,12 @@ use serenity::{
         prelude::Ready,
     },
     prelude::*,
+    Client as DiscordClient,
+};
+
+use irc::{
+    client::{data::Config, Client as IrcClient, Sender},
+    proto::Command,
 };
 
 struct Handler;
@@ -37,11 +44,11 @@ impl EventHandler for Handler {
             (http, channel_id, user_id)
         };
 
-        if user_id != msg.author.id && !msg.author.bot {
-            send_message(&http, &channel_id, &format!("{}: {}", nick, msg.content))
-                .await
-                .unwrap();
-        }
+        // if user_id != msg.author.id && !msg.author.bot {
+        //     send_message(&http, &channel_id, &format!("{}: {}", nick, msg.content))
+        //         .await
+        //         .unwrap();
+        // }
     }
 
     async fn ready(&self, ctx: Context, info: Ready) {
@@ -55,6 +62,7 @@ impl EventHandler for Handler {
 struct HttpKey;
 struct ChannelIdKey;
 struct UserIdKey;
+struct SenderKey;
 
 impl TypeMapKey for HttpKey {
     type Value = Arc<Http>;
@@ -68,29 +76,64 @@ impl TypeMapKey for UserIdKey {
     type Value = UserId;
 }
 
+impl TypeMapKey for SenderKey {
+    type Value = Sender;
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let token = env::var("DISCORD_TOKEN").expect("DISCORD_TOKEN not set");
 
-    let mut client = Client::builder(&token).event_handler(Handler).await?;
+    let mut discord_client = DiscordClient::builder(&token)
+        .event_handler(Handler)
+        .await?;
 
     let channel_id = ChannelId(831255708875751477);
 
+    let config = Config {
+        nickname: Some("dircord".to_string()),
+        server: Some("192.168.1.28".to_owned()),
+        port: Some(6667),
+        channels: vec!["#no-normies".to_string()],
+        use_tls: Some(false),
+        umodes: Some("+B".to_string()),
+        ..Config::default()
+    };
+
+    let mut irc_client = IrcClient::from_config(config).await?;
+
+    let http = discord_client.cache_and_http.http.clone();
+
     {
-        let mut data = client.data.write().await;
-        data.insert::<HttpKey>(client.cache_and_http.http.clone());
+        let mut data = discord_client.data.write().await;
+        data.insert::<HttpKey>(http.clone());
         data.insert::<ChannelIdKey>(channel_id);
+        data.insert::<SenderKey>(irc_client.sender());
     }
 
-    client.start().await?;
+    tokio::spawn(async move {
+        irc_loop(irc_client, http, channel_id).await.unwrap();
+    });
+    discord_client.start().await?;
 
     Ok(())
 }
 
-async fn send_message(
-    http: &Http,
-    channel_id: &ChannelId,
-    content: &str,
-) -> anyhow::Result<Message> {
-    Ok(channel_id.say(http, content).await?)
+async fn irc_loop(
+    mut client: IrcClient,
+    http: Arc<Http>,
+    channel_id: ChannelId,
+) -> anyhow::Result<()> {
+    client.identify()?;
+    let mut stream = client.stream()?;
+    while let Some(orig_message) = stream.next().await.transpose()? {
+        print!("{}", orig_message);
+        if let Command::PRIVMSG(ref channel, ref message) = orig_message.command {
+            let nickname = orig_message.source_nickname().unwrap();
+            channel_id
+                .say(&http, format!("{}: {}", nickname, message))
+                .await?;
+        }
+    }
+    Ok(())
 }
